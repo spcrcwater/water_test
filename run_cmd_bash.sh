3dfdca93875d6fe11963cf5fdbfbd0820f39aab0
AyushLall5 <ayushlall26@gmail.com>
2021-10-30 15:47:03 +0530

    update_30_oct_21

--- run_cmd_bash.sh
+++ run_cmd_bash.sh
@@ -2,9 +2,17 @@
 
 wget -q --spider http://google.com
 
+cicomid=0
 if [ $? -eq 0 ]; then
     cd /home/pi/Desktop/waterspcrc/Ph-03/
     git pull origin main
+    cicomid_new=$(git rev-parse HEAD)
+    if[ cicomid -ne cicomid_new]; then
+      sudo systemctl restart codetest.service
+    else
+      echo 'No changes'
+    fi
+
 else
     echo 'No Internet'
 fi
