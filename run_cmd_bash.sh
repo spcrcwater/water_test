2df42ff8a2456d2b088d130e02d9cf277984bd4b
AyushLall5 <ayushlall26@gmail.com>
2021-10-30 16:43:22 +0530

    update_30_oct_21

--- run_cmd_bash.sh
+++ run_cmd_bash.sh
@@ -3,11 +3,13 @@
 wget -q --spider http://google.com
 
 cicomid=$(git rev-parse HEAD)
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 ]
+ then
     cd /home/pi/Desktop/waterspcrc/Ph-03/
     git pull origin main
     cicomid_new=$(git rev-parse HEAD)
-    if[ cicomid -ne cicomid_new]; then
+    if[ cicomid -ne cicomid_new]
+    then
       cicomid = cicomid_new
       sudo systemctl restart codetest.service
     else
